69c3080ab130bbec74a99a8429098b422775eb2a
shin donggyu <donggyu9410@gmail.com>
2019-10-22 20:46:47 +0900

    asd

---
+++ build/precache-manifest.e8ffc9d387bafc6e7549ad79cda71751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8910c03eb85b363e83b9126a3cd4becc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be4bb21cbbce183db5e5",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "685d6132a4159517f84c",
+    "url": "/static/css/main.8a65e4e8.chunk.css"
+  },
+  {
+    "revision": "be4bb21cbbce183db5e5",
+    "url": "/static/js/2.a3cb3f6e.chunk.js"
+  },
+  {
+    "revision": "685d6132a4159517f84c",
+    "url": "/static/js/main.e86b51b2.chunk.js"
+  },
+  {
+    "revision": "5a77a9200fa308e56fc0",
+    "url": "/static/js/runtime-main.8c3aebd6.js"
+  },
+  {
+    "revision": "b47f7b0e910e7ff1ab629e3cdc013b2b",
+    "url": "/static/media/leo.b47f7b0e.png"
+  }
+]);
