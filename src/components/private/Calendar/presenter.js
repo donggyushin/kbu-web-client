67382b1f8143f6e52e4df0e718995be46876da79
shin donggyu <donggyu9410@gmail.com>
2019-11-26 21:04:16 +0900

    calendar done

--- src/components/private/Calendar/presenter.js
+++ src/components/private/Calendar/presenter.js
@@ -24,6 +24,7 @@ export default function ({ onNavigation, events, onEventSelected, event, detailV
             onSelectEvent={onEventSelected}
             eventPropGetter={eventPropGetter}
             onNavigate={onNavigation}
+            popup={true}
         />
         {detailView && <CalendarDetailView outsideOfDetailViewClicked={outsideOfDetailViewClicked} event={event} />}
         {/* <iframe src="https://calendar.google.com/calendar/embed?src=4tjtr23q6bcevh6no8q040eue8%40group.calendar.google.com&ctz=Asia%2FSeoul" style={{ border: "0" }} width="100%" height="600" frameBorder="0" scrolling="no"></iframe> */}
